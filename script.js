85f2add0249a36016e6c17525ad2108af3f81333
Wahome <lightbeam2007@gmail.com>
2025-01-25 19:16:53 +0300

    7th commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+const use ={
+    name: '',
+    UsEr:''
+}
+
+function addUser(user,password,email,date,gender){
+
+    let password = document.getElementById(pass)
+    let email= document.getElementById(email)
+    let gender= document.getElementById(radio)
+   
+     
+    
+
+    use.UsEr.push(addUser)
+
+    console.log(`User ${usered.name} has been added to the website`)
+}
+
+addUser(`name`,`password`,`email`,`date`,`gender`)
+
+
+
